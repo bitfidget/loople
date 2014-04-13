06cb8a09190c7e3bc9dafa175bbd28c2bd4d33a8
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-13 20:15:21 +1000

    user logins added and some basic nav and display pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,50 @@
 class UsersController < ApplicationController
+  before_filter :check_if_logged_in, :except => [:show, :new, :create]
+
   def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
+    @users = User.all
   end
 
   def show
+    @user = User.find params[:id]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find params[:id]
   end
 
   def update
+    @user = User.find params[:id]
+    @user.update_attributes params[:user]
+    redirect_to @user
   end
 
   def destroy
+    user = User.find params[:id]
+    user.destroy
+    redirect_to users_path, :notice => "user deleted"
+  end
+
+  private
+  def check_if_logged_in
+    redirect_to(root_path) if @current_user.nil?
+  end
+  
+  def check_if_admin
+    redirect_to(root_path) if @current_user.nil? || @current_user.admin == false
   end
 end
