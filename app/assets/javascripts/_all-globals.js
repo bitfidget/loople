4915f3039d2c32b182af8d143f164f530c9a02f5
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-16 17:32:16 +1000

    some quick comments added

--- app/assets/javascripts/_all-globals.js
+++ app/assets/javascripts/_all-globals.js
@@ -3,7 +3,7 @@
 // ----------------------------------------------------------------------------------
 
 // this will be the window for all animation
-var $animWindow = 0
+var $animWindow = 0;
 
 // KH all keypresses are saved in the loopKeyTime array, under 2 different arrays
 // KH loopKeys (the key pressed) and loopTimes (the time at which it was pressed)
@@ -11,4 +11,7 @@ var loopKeys = [];
 var loopTimes = [];
 
 // KH the global timer - everythign hangs off this variable!
-var countBar = 0
+var countBar = 0;
+
+// KH the load loop button nav
+var $loadLoops = 0;
--- app/assets/javascripts/loop-load.js
+++ app/assets/javascripts/loop-load.js
@@ -1,3 +1,15 @@
 //------------------------------
 // to load a saved loop via ajax
 //------------------------------
+
+var $loadLoops = 0;
+
+$(document).ready(function(){
+  $loadLoops = $('#loops-saved');
+  $loadLoops.on('click' , '.load-loop', function(event){
+    event.preventDefault();
+    var loopToLoad = this.class
+    console.log(loopToLoad);
+  });
+});
+
