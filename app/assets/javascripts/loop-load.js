baf4e174a8373093ec04f3e656b2c912d861dcdd
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-19 13:18:31 +1000

    basic ajax retreive going on now - need to do stuff with that info now

--- app/assets/javascripts/loop-load.js
+++ app/assets/javascripts/loop-load.js
@@ -1,39 +1,16 @@
 //------------------------------
-// to load a saved loops via ajax
+// to load a saved loops via ajax just the button listeners are here for now...
 //------------------------------
 
-loadLoops = function(){
-  // ajax to retrieve all of the loops belonging to current user
-  $.ajax({
-    url: '/loops/load',
-    type: 'GET',
-    dataType: 'json'
-  }).done(function(loops_to_load){
-    console.log(loops_to_load)
-    $.each(loops_to_load, function(index, loop){
-
-
-    }
-
-
-    .each(tasks, function (task) {
-      task.created_at = moment(task.created_at).calendar();
-      task.updated_at = moment(task.updated_at).calendar();
-      var html = task_html(task);
-      if (task.complete) {
-        $(html).appendTo('#completed');
-      } else {
-        $(html).appendTo('#tasks');
-      }
-    });
-  }).fail(function(){
-    console.log('fart - load loops failed');
-  });
-};
-
 $(document).ready(function(){
-  loadLoops()
+  $loopNav = $('#loop-nav');
+  loopAjax.loadLoops();
+  $loopNav.on('click', '.load-loop', function(){
+    var id = $(this).closest('li').data('id')
+    console.log('play ' + id)
+  });
+  $loopNav.on('click', '.delete-loop', function(){
+    var id = $(this).closest('li').data('id')
+    console.log('delete ' + id)
+  });
 });
-
-
-// need to make the button clicked get its ID so we can load the loop
--- app/assets/javascripts/loop-save.js
+++ app/assets/javascripts/loop-save.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
 
 // KH create an object to deal with all Ajax Loop actions
 var loopAjax = {
-  createLoop: function(loopName, loopColour, loopKeysTimes) {
+  createLoop: function(loopName, loopColour, loopKeysTimes){
+    // ajax to save the new loop to the db
     console.log(loopKeysTimes);
     $.ajax({
       url: '/loops',
@@ -12,10 +13,45 @@ var loopAjax = {
       dataType: 'json',
       // data: {loop: { name: loopName, colour: loopColour }}
       data: {loop: { name: loopName, colour: loopColour }, loopKeysTimes: loopKeysTimes}
-    }).done(function(){
+    })
+
+    // if ajax is a success do the following
+    .done(function(){
       console.log('loop created');
-    }).fail(function(){
+    })
+
+    // if ajax fails, fart
+    .fail(function(){
       console.log('fart');
+    });
+  },
+
+  loadLoops: function(){
+    // ajax to retrieve all of the loops belonging to current user
+    $.ajax({
+      url: '/loops/load',
+      type: 'GET',
+      dataType: 'json'
+    })
+
+    // if ajax is a success do the following
+    .done(function(loops_to_load){
+      $.each(loops_to_load, function(index, loop){
+        console.log(this.name)
+        console.log(this.id)
+        console.log(this.modified)
+        console.log(this.time)
+        var navItem = "<li data-id=" + this.id + "><a href='#' class='delete-loop'>X</a><a href='#' class='load-loop'><span class='queue'>cue</span>" + this.name + "</a></li>"
+        $loopNav.append(navItem);
+      })
+    })
+
+    // if ajax is fails, do the usual
+    .fail(function(){
+      console.log('fart - load loops failed');
     });
   }
 };
+
+
+
