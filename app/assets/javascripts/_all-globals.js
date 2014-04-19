f8a5213c48aa7d0b634440b3cedb7c24b4b6bb6d
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-19 14:40:20 +1000

    huge refactor

--- app/assets/javascripts/_all-globals.js
+++ app/assets/javascripts/_all-globals.js
@@ -2,8 +2,19 @@
 // these are all variables that will need to be used by more than one function
 // ----------------------------------------------------------------------------------
 
-// this will be the window for all animation
+// window width and height
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
+
+// the main windows
 var $animWindow = 0;
+var $loopWindow = 0;
+
+// the loop head
+var $loopHead = 0;
+
+// the navigation for saved loops
+var $loopNav = 0;
 
 // KH all keypresses are saved in the loopKeysTimes hash
 // KH the time is the key, and the keys played are the value
@@ -12,5 +23,12 @@ var loopKeysTimes = {};
 // KH the global timer - everythign hangs off this variable!
 var countBar = 0;
 
+// KH the global looptime - start with 2000 (120BPM)
+var loopTime = 2000
+
 // KH the load loop button nav
 var $loadLoops = 0;
+
+// the number of steps on the metronome/grid
+var steps = 4;
+var grid = 0;
--- app/assets/javascripts/loop-load.js
+++ app/assets/javascripts/loop-load.js
@@ -2,17 +2,3 @@
 // to load a saved loops via ajax just the button listeners are here for now...
 //------------------------------
 
-$(document).ready(function(){
-  $loopNav = $('#loop-nav');
-  loopAjax.loadLoops();
-  $loopNav.on('click', '.load-loop', function(){
-    var id = $(this).closest('li').data('id')
-    console.log('play ' + id)
-  });
-  $loopNav.on('click', '.delete-loop', function(){
-    var id = $(this).closest('li').data('id');
-    $(this).closest('li').fadeOut(function(){
-      loopAjax.deleteLoop(id);
-    });
-  });
-});
