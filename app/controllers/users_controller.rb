0b201150bf2abffcb8a7891d0a2dcd504b110bba
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-23 15:33:21 +1000

    fixing new user path redirect garrrrr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new params[:user]
     if @user.save
       session[:user_id] = @user.id
-      redirect_to index
+      redirect_to root_path
     else
       render :new
     end
