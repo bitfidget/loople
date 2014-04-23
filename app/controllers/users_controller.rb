240ed895c98df722064b0181adcd4698cb2e2e25
=-kriss-= <kriss@modelcitizen.com.au>
2014-04-23 15:40:39 +1000

    fixing new user path redirect more garrrrr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     @user.update_attributes params[:user]
-    redirect_to @user
+    redirect_to root_path
   end
 
   def destroy
